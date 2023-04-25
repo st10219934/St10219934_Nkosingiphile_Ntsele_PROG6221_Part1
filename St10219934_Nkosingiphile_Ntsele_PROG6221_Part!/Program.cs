64fa0985ed5cd4e2ec6978ea2e9f4792f980998c
Nkosi-ngiphile <st10219934@rcconnect.edu.za>
2023-04-25 20:50:14 +0000

    Added comments on the main method

--- St10219934_Nkosingiphile_Ntsele_PROG6221_Part!/Program.cs
+++ St10219934_Nkosingiphile_Ntsele_PROG6221_Part!/Program.cs
@@ -10,19 +10,26 @@ namespace St10219934_Nkosingiphile_Ntsele_PROG6221_Part_
     {
         static void Main(string[] args)
         {
-
+            //Changing the color of the text 
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.BackgroundColor = ConsoleColor.Black;
 
 
-
+            //making an object for the external class to b e able to call the methods in it
             RecipeInformation obj = new RecipeInformation();
 
+
+            //intialized and declared a loop control varaible for the do while loop
             int i = 0;
+
+            //this is the do while loop for prompting the user for options
             do
             {
 
+                // declared a variable to stored the user's input for an option chosen
                 int choose;
+
+                //the menu for the user to choose from
                 Console.WriteLine("Choose the options below \n" +
                                   "1 to write the recipe \n" +
                                   "2 to scale all the quantities \n" +
@@ -30,29 +37,37 @@ namespace St10219934_Nkosingiphile_Ntsele_PROG6221_Part_
                                   "4 to print and see the recipe \n" +
                                   "5 to clear all data and write a new recipe ");
 
-
+                //initialized the variable for storing user's option chosen
                 choose = Convert.ToInt32(Console.ReadLine());
 
 
+                // if and else statement for the application to know which number is chosen by user
                 if (choose == 1)
                 {
+                    //calling the method that prompt theuser for recipe details
                     obj.recipeDetails();
+
+                    //calling the method that prompt the user for a number of stepd and its description
                     obj.stepDetails();
                 }
                 else if (choose == 2)
                 {
-                     obj.scaleQuantity();
+                    //calling the method that prompt the user if they want to scale the quantity
+                    obj.scaleQuantity();
                 }
                 else if (choose == 3)
                 {
+                    //calling the method that prompt the user if they want to reset the quantity back to it's origin
                     obj.resetQuantity();
                 }
                 else if (choose == 4)
                 {
+                    //calling the method that prints the recipe entered by the user
                     obj.printRecipe();
                 }
                 else if (choose == 5)
                 {
+                    //calling the method that prompt the user if they want to clear all the data and write a new recipe
                     obj.clearAllData();
                 }
             } while (i <= 5);
