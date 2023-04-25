42d1e42def95f36a741942de987de5a755a0accb
Nkosi-ngiphile <st10219934@rcconnect.edu.za>
2023-04-25 21:09:45 +0000

    Added comments in the external class in the following methods (recipeDetails,stepDetails,scaleQuantity)

--- St10219934_Nkosingiphile_Ntsele_PROG6221_Part!/RecipeInformation.cs
+++ St10219934_Nkosingiphile_Ntsele_PROG6221_Part!/RecipeInformation.cs
@@ -8,34 +8,33 @@ namespace St10219934_Nkosingiphile_Ntsele_PROG6221_Part_
 {
     internal class RecipeInformation
     {
+
+        //declared all the variables for storing the user's input
         static string[] nameOfIngredient;
         static double[] quantity;
         static string[] unitMeasure;
         static int numberOfSteps;
         static string[] Description;
+        static double[] originalQuantity;
+        static double scaleFactor;
 
-        double[] originalQuantity;
-        double scaleFactor;
-
-
+        //declared a variable to hold a number of ingredients
         static int numberOfIngredients;
-
-
-
-        // this is a method for saving the recipe ingredients
-
+        
         public void recipeDetails()
         {
-
+            //asking the user on how many ingredients they want to capture
             Console.WriteLine("How many ingredients do you want to write ");
             numberOfIngredients = Convert.ToInt32(Console.ReadLine() + "\n");
 
+            //initializing all the variable on how many times they should store details according to the user 
             nameOfIngredient = new string[numberOfIngredients];
             quantity = new double[numberOfIngredients];
             unitMeasure = new string[numberOfIngredients];
             int numberOfSteps;
 
 
+            //this for loop is for asking the user for recipe details according to the number of recipes they want
             for (int g = 0; g < numberOfIngredients; g++)
             {
                 Console.Write("\n Write the ingredient name " + (g + 1) + "\n");
@@ -45,16 +44,21 @@ namespace St10219934_Nkosingiphile_Ntsele_PROG6221_Part_
                 Console.WriteLine("Unit measure");
                 unitMeasure[g] = Console.ReadLine();
             }
-            originalQuantity = quantity;
+            originalQuantity = quantity;//initializing the variable originalQuantity so that we will be able to reset the quantity
         }
+
+        //method for asking the number of steps and capturing the descriptions for the steps
         public void stepDetails()
         {
 
+            //asking the user on how many steps do they wish to write
             Console.WriteLine("\n Number of steps");
             numberOfSteps = Convert.ToInt32(Console.ReadLine());
 
-
+            //variable for holding the description entered by user
             Description = new string[numberOfSteps];
+
+            //this for loop is for the application to know how many steps to be written by the user
             for (int i = 0; i < numberOfSteps; i++)
             {
 
@@ -62,13 +66,19 @@ namespace St10219934_Nkosingiphile_Ntsele_PROG6221_Part_
                 Description[i] = Console.ReadLine();
             }
         }
+
+        // method for scalling the quantity of all the ingredients
         public void scaleQuantity()
         {
+            //asking the user on how much they want to scale their quantities to
             Console.WriteLine("How much do you want to scale your quantities");
             scaleFactor = Convert.ToDouble(Console.ReadLine());
 
+            //this loop is for scalling all the quantities with the number entered by user to scale with
             for (int i = 0; i < quantity.Length; i++)
             {
+                //this is where the scalling happen
+                //the quantity is multiplied by the number entered by user to scale with
                 Console.WriteLine(quantity[i] *= scaleFactor);
             }
         }
