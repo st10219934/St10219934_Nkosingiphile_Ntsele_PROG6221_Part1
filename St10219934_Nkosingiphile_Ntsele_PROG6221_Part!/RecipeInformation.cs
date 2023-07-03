b1ee3315df07b966fa9ed71416d1a6d1f2937bb8
Nkosi-ngiphile <st10219934@rcconnect.edu.za>
2023-07-03 09:05:34 +0000

    added a new window to scale the quantities

--- St10219934_Nkosingiphile_Ntsele_PROG6221_Part!/RecipeInformation.cs
+++ St10219934_Nkosingiphile_Ntsele_PROG6221_Part!/RecipeInformation.cs
@@ -30,133 +30,33 @@ namespace St10219934_Nkosingiphile_Ntsele_PROG6221_Part_
 
         // this is a method for saving the recipe ingredients
 
-        public void recipeDetails()
-        {
-
-
-
-
-            // Prompt the user to enter the number of recipes
-            Console.WriteLine("How many recipes do you want to enter");
-            numberOfRecipes = Convert.ToInt32(Console.ReadLine());
-
-
-
-
-            // Loop to capture details for each recipe
-            for (int i = 0; i < numberOfRecipes; i++)
-            {
-
-                // Prompt the user to enter the name of the recipe
-                Console.WriteLine("\nEnter the  name of the recipe");
-                namesOfTheRecipe.Add(Console.ReadLine());
-
-                // Add a new recipe entry to the storeRecipes dictionary
-                storeRecipes.Add(namesOfTheRecipe[incr], new Dictionary<int, string>());
-
-                // Prompt the user to enter the number of ingredients for the current recipe
-                Console.WriteLine("\nHow many ingredients do you want to capture");
-                numberOfIngredients = Convert.ToInt32(Console.ReadLine());
-
-
-                // Create a new array to store the quantities,food groups,  unit measures of ingredients
-                quantity = new double[numberOfIngredients];
-                unitMeasure = new string[numberOfIngredients];
-                foodGroup = new string[numberOfIngredients];
-
-
-
-                // Loop to capture details for each ingredient
-                for (int x = 0; x < numberOfIngredients; x++)
-                {
-                    // Prompt the user to enter the name of the ingredient
-                    Console.WriteLine("\nEnter name of the ingredient" + (x + 1));
-                    ingredients.Add(Console.ReadLine());
-
-
-                    // Prompt the user to enter the quantity of the ingredient
-                    Console.WriteLine("\nEnter the  quantity");
-                    quantity[x] = Convert.ToDouble(Console.ReadLine());
-
-
-                    // Prompt the user to enter the unit measure of the ingredient
-                    Console.WriteLine("\nEnter the unit measure");
-                    unitMeasure[x] = Console.ReadLine();
-
-
-                    // Prompt the user to enter the food group of the ingredient
-                    Console.WriteLine("\nEnter the food group");
-                    foodGroup[x] = Console.ReadLine();
-
-
-                    // Prompt the user to enter the number of calories for the ingredient
-                    Console.WriteLine("\nEnter the  number of colories");
-                    colories.Add(Convert.ToInt32(Console.ReadLine()));
-
-
-                    // Add the ingredient details to the current recipe entry in the storeRecipes dictionary
-                    storeRecipes[namesOfTheRecipe[incr]].Add(i,
-                        "\n________________________________________\n" +
-                        "          Here is the recipe\n" +
-                        "________________________________________\n" +
-                        "Ingredients\n" +
-                        "***********" +
-                        "\nName: " + ingredients[x] +
-                        "\nQuantity: " + quantity[x] +
-                        "\nUnit of measurement: " + unitMeasure[x] +
-                        "\nFood group: " + foodGroup[x] +
-                        "\nCalories: " + colories[x]);
-                }
-
-
-                // Add a new entry for the steps of the current recipe in the storeSteps dictionary
-                storeSteps.Add(namesOfTheRecipe[incr], new Dictionary<int, string>());
-
-
-                // Prompt the user to enter the number of steps for the current recipe
-                int numberOfSteps;
-                Console.WriteLine("\nHow many steps do you want to write");
-                numberOfSteps = Convert.ToInt32(Console.ReadLine());
-
-
-                // Loop to capture details for each step
-                for (int y = 0; y < numberOfSteps; y++)
-                {
-
-                    // Prompt the user to enter the description for the current step
-                    Console.WriteLine("Enter the description for step no. " + (y + 1));
-                    steps.Add(Console.ReadLine());
-
-                    // Create the step description using the entered description and step number
-                    string stepDescription = "Step no." + (y + 1) + ": " + steps[y];
-
-
-                    // Add the step description to the current recipe entry in the storeSteps dictionary
-                    if (storeSteps.ContainsKey(namesOfTheRecipe[incr]))
-                    {
-                        storeSteps[namesOfTheRecipe[incr]].Add(y, stepDescription);
-                    }
-                    else
-                    {
-                        var stepDictionary = new Dictionary<int, string>();
-                        stepDictionary.Add(y, stepDescription);
-                        storeSteps.Add(namesOfTheRecipe[incr], stepDictionary);
-                    }
-                }
-
-                // Increment the counter for recipe entries
-                incr++;
-
-                // Store the original quantity for the current recipe
-                originalQuantity = quantity;
-            }
-
-
-
-        }
+      
 
         // method for scalling the quantity of all the ingredients
-   
+        public void scaleQuantity()
+        {
+
+            // Prompt the user to enter the scale factor for quantity scaling
+            Console.WriteLine("How much do you want to scale your quantities");
+            scaleFactor = Convert.ToDouble(Console.ReadLine());
+
+            // Create a new array to store the scaled quantities
+            scaledQuantity = new double[quantity.Length];
+
+
+            // Loop through the original quantity array and scale each quantity
+            for (int i = 0; i < quantity.Length; i++)
+            {
+                scaledQuantity[i] = quantity[i] * scaleFactor;
+                quantity[i] = scaledQuantity[i];
+            }
+
+            // Inform the user that the quantities have been scaled
+            Console.WriteLine("Your quantities have been scaled.");
+
+            // Update the original quantity with the scaled quantity.
+            quantity = scaledQuantity;
+        }
 
         //method for resetting the quantity back to original 
         public void resetQuantity()
